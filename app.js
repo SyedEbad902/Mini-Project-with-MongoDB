4bec76c608df03df97a466ec1953ba6b1820673c
SyedEbad902 <ebad3088@gmail.com>
2024-10-26 01:46:25 +0500

    post added

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
+import bcrypt from "bcrypt";
 import cookieParser from "cookie-parser";
 import express from "express";
+import jwt from "jsonwebtoken";
 import userModel from "./models/user.js";
 import postModel from "./models/post.js";
-import jwt from 'jsonwebtoken'
-import bcrypt from 'bcrypt'
 const app = express();
 
 app.set("view engine", "ejs");
@@ -15,6 +15,38 @@ app.get("/", (req, res) => {
   console.log("Request received at /");
 
   res.render("index");
+});
+
+app.get("/login", (req, res) => {
+  console.log("Request received at /login");
+
+  res.render("login");
+});
+
+app.get("/logout", (req, res) => {
+  res.cookie("token", "");
+  res.redirect("/login");
+});
+
+app.get("/profile", isLoggedin,async (req, res) => { 
+  // console.log(req.user);
+
+  let user = await userModel.findOne({ email: req.user.email }).populate('posts');
+  res.render("profile", { user: user });
+  
+});
+
+app.post("/post", isLoggedin, async (req, res) => { 
+
+  let user =await userModel.findOne({email : req.user.email})
+  
+  let post = await postModel.create({
+    user: user._id,
+    content : req.body.content
+  });
+  user.posts.push(post._id);
+  await user.save();
+  res.redirect('/profile');
 });
 
 app.post("/register", async (req, res) => {
@@ -35,11 +67,41 @@ app.post("/register", async (req, res) => {
         });
         let token = jwt.sign({ email: email, userid: user._id }, "shhhhh");
         res.cookie("token", token);
-        res.send("Registered");
+        res.redirect("/login");
       });
     });
   }
 });
+
+app.post("/login", async (req, res) => {
+  let { email, password } = req.body;
+
+  let user = await userModel.findOne({ email: email });
+  if (!user) {
+    return res.status(500).send("Something went wrong");
+  } else {
+    bcrypt.compare(password, user.password, (err, result) => {
+      if (result) {
+        let token = jwt.sign({ email: email, userid: user._id }, "shhhhh");
+        res.cookie("token", token);
+        res.status(200).redirect("profile");
+      } else {
+        res.redirect("/login");
+      }
+    });
+  }
+});
+
+function isLoggedin(req, res, next) {
+  if (req.cookies.token === "") {
+    res.status(500).redirect("/login");
+  } else {
+    let data = jwt.verify(req.cookies.token, "shhhhh");
+    req.user = data;
+    next();
+  }
+}
+
 
 app.listen(4000, () => {
   console.log("Server is running on http://localhost:4000");
